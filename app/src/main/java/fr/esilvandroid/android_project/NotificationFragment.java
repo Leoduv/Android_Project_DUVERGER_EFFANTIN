570e6261862f7f8584c1a0de0e894967518884de
Leoduv <leopold.duvergernoinski@edu.devinci.fr>
2020-03-25 08:55:03 +0100

    corrected image file size and website display

--- app/src/main/java/fr/esilvandroid/android_project/NotificationFragment.java
+++ app/src/main/java/fr/esilvandroid/android_project/NotificationFragment.java
@@ -1,5 +1,7 @@
 package fr.esilvandroid.android_project;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NotificationFragment extends Fragment {
+
+    Button btnconseil;
+    Button btnajouter;
+    Button btntodolist;
 
 
     public NotificationFragment() {
@@ -24,7 +31,43 @@ public class NotificationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_notification, container, false);
+
+        btnconseil = rootView.findViewById(R.id.button_conseil);
+        btnajouter = rootView.findViewById(R.id.button_add);
+        btntodolist = rootView.findViewById(R.id.button_todolist);
+
+        btnconseil.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getActivity(), ConseilActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnajouter.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getActivity(), AddOfferActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btntodolist.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getActivity(), RetrofitActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
     }
 
 }
